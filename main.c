7b4af181c0c18845a5e1786a53a786e1706610c1
Miika Piiroinen <miika.piiroinen@aalto.fi>
2018-02-25 17:59:20 +0200

    refactoring. Busy loop delay function

--- main.c
+++ main.c
@@ -1,125 +1,78 @@
-// Modified from https://github.com/jgarff/rpi_ws281x
-
-// Access from ARM Running Linux
- 
 #define BCM2708_PERI_BASE        0x20000000
 #define GPIO_BASE                (BCM2708_PERI_BASE + 0x200000) /* GPIO controller */
- 
-
 #include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <sys/mman.h>
 #include <unistd.h>
- 
 #define PAGE_SIZE (4*1024)
 #define BLOCK_SIZE (4*1024)
- 
 int  mem_fd;
 void *gpio_map;
- 
-// I/O access
 volatile unsigned *gpio;
- 
- 
-// GPIO setup macros. Always use INP_GPIO(x) before using OUT_GPIO(x) or SET_GPIO_ALT(x,y)
 #define INP_GPIO(g) *(gpio+((g)/10)) &= ~(7<<(((g)%10)*3))
 #define OUT_GPIO(g) *(gpio+((g)/10)) |=  (1<<(((g)%10)*3))
 #define SET_GPIO_ALT(g,a) *(gpio+(((g)/10))) |= (((a)<=3?(a)+4:(a)==4?3:2)<<(((g)%10)*3))
- 
-#define GPIO_SET *(gpio+7)  // sets   bits which are 1 ignores bits which are 0
-#define GPIO_CLR *(gpio+10) // clears bits which are 1 ignores bits which are 0
- 
-#define GET_GPIO(g) (*(gpio+13)&(1<<g)) // 0 if LOW, (1<<g) if HIGH
- 
-#define GPIO_PULL *(gpio+37) // Pull up/pull down
-#define GPIO_PULLCLK0 *(gpio+38) // Pull up/pull down clock
+#define GPIO_SET *(gpio+7)
+#define GPIO_CLR *(gpio+10)
+#define GET_GPIO(g) (*(gpio+13)&(1<<g))
+#define GPIO_PULL *(gpio+37)
+#define GPIO_PULLCLK0 *(gpio+38)
  
 void setup_io();
-inline long int get_cycle_time();
-void update(int* gpio_set, int* gpio_clr, int n);
- 
-int main(int argc, char **argv)
-{
+inline long int get_one_cycle_ns();
+inline void update(int* gpio_set, int* gpio_clr, int n) { for(int i; i < n; i++) { GPIO_SET = gpio_set[i]; GPIO_CLR = gpio_clr[i]; } }
+inline void delay_ns(int t_ns, int one_cycle_ns) { int n = (time_ns / one_cycle_ns - 5); delay_cycles(n); }
+inline void delay_cycles(int n) { for(int i = 0; i < n; ++i) {} }
 
-
+int main(int argc, char **argv) {
   int g,rep;
- 
-  // Set up gpi pointer for direct register access
   setup_io();
- 
-  // Switch GPIO 7..11 to output mode
- 
- /************************************************************************\
-  * You are about to change the GPIO settings of your computer.          *
-  * Mess this up and it will stop working!                               *
-  * It might be a good idea to 'sync' before running this program        *
-  * so at least you still have your code changes written to the SD-card! *
- \************************************************************************/
- 
-  // Set GPIO pins 7-11 to output
-  for (g=7; g<=11; g++)
-  {
-    INP_GPIO(g); // must use INP_GPIO before we can use OUT_GPIO
+  for (g=7; g<=11; g++) {
+    INP_GPIO(g);
     OUT_GPIO(g);
   }
-
-  long int t_cycle = get_cycle_time();
- 
+  long int t_cycle = get_one_cycle_ns();
   return 0;
- 
-} // main
+}
 
-long int get_cycle_time() {
+
+inline get_one_cycle_ns() {
     int gpio_set[10000] = { 0 };
     int gpio_clr[10000] = { 0 };
-    int n = 10000;
+    int n = 100000;
     printf("timing %d gpio updates...\n", n);
     struct timespec t0;
     struct timespec t1;
     clock_gettime(CLOCK_MONOTONIC, &t0);
-    update(gpio_set, gpio_clr, 10000);
+    delay_cycles(n);
     clock_gettime(CLOCK_MONOTONIC, &t1);
     long int dt = t1.tv_nsec - t0.tv_nsec;
     long int t_cycle = dt / n;
-    printf("time for %d gpio updates (dt): %ld ns\n", n, dt);
-    printf("time for one cycle/update (t_cycle): %ld ns\n", t_cycle);
+    printf("delay_cycles(%d): %ld ns\n", n, dt);
+    printf("one_cycle: %ld ns\n", t_cycle);
     return dt/n;
 }
-
-inline void update(int* gpio_set, int* gpio_clr, int n) {
-    for(int i; i < n; i++) {
-        GPIO_SET = gpio_set[i];
-        GPIO_CLR = gpio_clr[i];
-    }
-}
  
  
-//
-// Set up a memory regions to access GPIO
-//
-void setup_io()
-{
-    /* open /dev/mem */
+void setup_io() {
     if ((mem_fd = open("/dev/mem", O_RDWR|O_SYNC) ) < 0) {
        printf("can't open /dev/mem \n");
        exit(-1);
     }
-    /* mmap GPIO */
     gpio_map = mmap(
-       NULL,             //Any adddress in our space will do
-       BLOCK_SIZE,       //Map length
-       PROT_READ|PROT_WRITE,// Enable reading & writting to mapped memory
-       MAP_SHARED,       //Shared with other processes
-       mem_fd,           //File to map
-       GPIO_BASE         //Offset to GPIO peripheral
+       NULL,
+       BLOCK_SIZE,
+       PROT_READ|PROT_WRITE,
+       MAP_SHARED,
+       mem_fd,
+       GPIO_BASE
     );
-    close(mem_fd); //No need to keep mem_fd open after mmap
+    close(mem_fd);
     if (gpio_map == MAP_FAILED) {
-       printf("mmap error %d\n", (int)gpio_map);//errno also set!
+       printf("mmap error %d\n", (int)gpio_map);
        exit(-1);
     }
-    // Always use volatile pointer!
     gpio = (volatile unsigned *)gpio_map;
-} // setup_io
+}
